f4c3d329135594c28983693392698816d0c04719
Emir Kovacevic <emirkovacevic@protonmail.com>
2021-08-26 15:49:50 +0200

    .

--- app/controllers/user.contoller.js
+++ app/controllers/user.contoller.js
@@ -155,16 +155,3 @@ exports.deleteAll = (req, res) => {
         });
 };
 
-// find all published Users
-exports.findAllPublished = (req, res) => {
-    User.findAll({where: {published: true}})
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving users."
-            });
-        });
-};
